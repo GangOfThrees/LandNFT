6e3392eae57fe416bf8a7f0de39a6e538ca5db4f
Elijah <elijahahianyo@gmail.com>
2023-10-28 17:52:20 +0000

    added layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+
+
+import { Flex, Spacer, Text, Box } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box bg={`bg.100`}>
+      //navbar
+    <Flex >
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </Flex>
+    <Flex bg={`bg.100`}>
+        <Flex>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <Flex bg={`primary.100`} direction={`column`} wrap={`nowrap`}>
+            <Text>Discover Digital Art & collect NFTs</Text>
+            <Text>NFT Marketplace UI created with Anima For Figma. Dollect, Buy and Sell Art From More than 20k NFT Artists.</Text>
+            <button>Get Started</button>
+          </Flex>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <Flex direction={`row`}>
+              <Text>240k+</Text>
+              <Text>240k+</Text>
+              <Text>240k+</Text>
+          </Flex>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+        </Flex>
+        <Flex>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </Flex>
+    </Flex>
+    </Box>
+   
   )
 }
---
+++ frontend/src/components/theme.tsx
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+    primary: {
+        100: "#A259FF"
+    },
+    bg: {
+        100: "#2B2B2B"
+    }
+}
+
+
+export const theme = extendTheme({
+    colors
+  })
